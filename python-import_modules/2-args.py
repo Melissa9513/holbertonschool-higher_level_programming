47e88b07a0dfdd8701b4999bff65c801018eec23
Melissa <melissasad201@gmail.com>
2026-01-16 22:45:19 +0000

    push

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-if **name** == "**main**":
 import sys
+if __name__ == "__main__":
+    ac = len(sys.argv) - 1
+    if ac == 0:
+        print("0 arguments.")
+    elif ac == 1:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(ac))
+    for i in range(1, ac + 1):
+        print("{:d}: {}".format(i, sys.argv[i]))
 
-```
-argc = len(sys.argv) - 1
-
-if argc == 0:
-    print("0 arguments.")
-elif argc == 1:
-    print("1 argument:")
-    print("1: {}".format(sys.argv[1]))
-else:
-    print("{} arguments:".format(argc))
-    for i in range(1, argc + 1):
-        print("{}: {}".format(i, sys.argv[i]))
-```
