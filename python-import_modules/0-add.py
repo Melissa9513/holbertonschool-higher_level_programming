4847455ec11f570dd8bb41783d50ff0134e03f36
Melissa9513 <melissasad201@gmail.com>
2026-01-16 17:45:53 +0100

    Fix main guard and format print statement

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-if **name** == "**main**":
 from add_0 import add
-
-```
-a = 1
-b = 2
-
-print("{} + {} = {}".format(a, b, add(a, b)))
-```
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
