89ef3098f8fd32e2f420e436da727558c20096b5
Melissa <melissasad201@gmail.com>
2026-02-03 19:08:14 +0000

    push

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-import this
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = "object-oriented programming with Python"
+print(str)
