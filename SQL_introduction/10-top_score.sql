08f0efcaddc40fc1e1084c24ee30e1b01c0db6d9
Melissa <melissasad201@gmail.com>
2026-03-04 23:45:18 +0000

    push

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- 10. List by best
+-- lists all records from second_table ordered by score (highest first)
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- 7. Insert value
+-- inserts a new row in the table first_table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
