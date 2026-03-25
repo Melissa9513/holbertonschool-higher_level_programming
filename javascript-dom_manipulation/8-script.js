7142619a788e8165bc7e99621a98936e4a45d500
Melissa <melissasad201@gmail.com>
2026-03-25 13:30:11 +0000

    push

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hello = document.querySelector("#hello");
+
+  fetch("https://hellosalut.stefanbohacek.com/?lang=fr")
+    .then(response => response.json())
+    .then(data => {
+      hello.textContent = data.hello;
+    })
+    .catch(error => console.log(error));
+});
